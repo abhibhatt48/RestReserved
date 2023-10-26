bd17836a2680bd6571a0638dc8e3b8d8c7a359a7
ASHISH OJHA <as950640@dal.ca>
2023-10-25 21:19:36 -0300

    updated frontend code

--- customer-app-frontend/src/views/Booking/MenuSelector.js
+++ customer-app-frontend/src/views/Booking/MenuSelector.js
@@ -1,23 +1,22 @@
 import React, { useState, useEffect } from "react";
 import {
-  Accordion,
-  AccordionSummary,
-  AccordionDetails,
-  Typography,
   Card,
   CardContent,
   CardMedia,
-  Button,
+  Typography,
   Grid,
   IconButton,
   Box,
   Container,
 } from "@mui/material";
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import AddIcon from "@mui/icons-material/Add";
 import RemoveIcon from "@mui/icons-material/Remove";
+import { useTheme } from "@mui/material/styles";
 
-function MenuSelector() {
+const MenuSelector = () => {
+  const theme = useTheme();
+  const cardWidth = "100%";
+
   const [restaurants, setRestaurants] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
@@ -55,63 +54,66 @@ function MenuSelector() {
       ) : (
         <Grid container spacing={2}>
           {restaurants.map((restaurant) => (
-            <Grid item xs={12} sm={6} md={4} lg={3} key={restaurant.MenuItemID}>
-              <Accordion>
-                <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-                  <Typography variant="h6">
-                    Restaurant ID: {restaurant.RestaurantID}
-                  </Typography>
-                </AccordionSummary>
-                <AccordionDetails>
-                  <div>
-                    {restaurant.Items.map((menuItem) => (
-                      <Card
-                        style={{ display: "flex", width: 300, margin: "8px" }}
-                        key={menuItem.ItemID}
-                      >
-                        <CardMedia
-                          component="img"
-                          alt={menuItem.ItemName}
-                          height="140"
-                          image={menuItem.ImageUrl}
-                        />
-                        <div style={{ flex: 1 }}>
-                          <CardContent>
-                            <Typography variant="h6">
-                              {menuItem.ItemName}
-                            </Typography>
-                            <Typography>{menuItem.Description}</Typography>
-                            <Typography>
-                              Price: ${parseFloat(menuItem.Price).toFixed(2)}
-                            </Typography>
-                            <Typography>
-                              Category: {menuItem.Category.join(", ")}
-                            </Typography>
-                            <Box display="flex" alignItems="center">
-                              <IconButton
-                                onClick={() => handleRemove(menuItem)}
-                              >
-                                <RemoveIcon />
-                              </IconButton>
-                              <Typography variant="h6">0</Typography>{" "}
-                              {/* Display quantity here */}
-                              <IconButton onClick={() => handleAdd(menuItem)}>
-                                <AddIcon />
-                              </IconButton>
-                            </Box>
-                          </CardContent>
-                        </div>
-                      </Card>
-                    ))}
-                  </div>
-                </AccordionDetails>
-              </Accordion>
+            <Grid
+              item
+              xs={12}
+              sm={6}
+              md={4}
+              lg={3}
+              key={restaurant.MenuItemID}
+              name="menuSelectorGrid"
+            >
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  flexWrap: "wrap",
+                }}
+                name="cardDiv"
+              >
+                {restaurant.Items.map((menuItem) => (
+                  <Card
+                    style={{ width: cardWidth, marginBottom: theme.spacing(2) }}
+                    key={menuItem.ItemID}
+                  >
+                    <CardMedia
+                      component="img"
+                      alt={menuItem.ItemName}
+                      height="140"
+                      image={menuItem.ImageUrl}
+                    />
+                    <div style={{ flex: 1 }}>
+                      <CardContent>
+                        <Typography variant="h6">
+                          {menuItem.ItemName}
+                        </Typography>
+                        <Typography>{menuItem.Description}</Typography>
+                        <Typography>
+                          Price: ${parseFloat(menuItem.Price).toFixed(2)}
+                        </Typography>
+                        <Typography>
+                          Category: {menuItem.Category.join(", ")}
+                        </Typography>
+                        <Box display="flex" alignItems="center">
+                          <IconButton onClick={() => handleRemove(menuItem)}>
+                            <RemoveIcon />
+                          </IconButton>
+                          <Typography variant="h6">0</Typography>{" "}
+                          <IconButton onClick={() => handleAdd(menuItem)}>
+                            <AddIcon />
+                          </IconButton>
+                        </Box>
+                      </CardContent>
+                    </div>
+                  </Card>
+                ))}
+              </div>
             </Grid>
           ))}
         </Grid>
       )}
     </Container>
   );
-}
+};
 
 export default MenuSelector;
--- customer-app-frontend/src/views/Booking/index.js
+++ customer-app-frontend/src/views/Booking/index.js
@@ -1,27 +1,87 @@
-import React, { useState } from "react";
-import { Checkbox, FormControlLabel } from "@mui/material";
-import BookingForm from "./BookingForm";
-import TimeSlotSelector from "./TimeSlotSelector";
-import MenuSelector from "./MenuSelector";
+import React, { useState, useEffect } from "react";
+import Grid from "@mui/material/Grid";
+import MenuItemCard from "./MenuItemCard";
+import Box from "@mui/material/Box";
+import Checkbox from "@mui/material/Checkbox";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Footer from "../../common/Footer";
 
 function Booking() {
-  const [isMenuSelected, setIsMenuSelected] = useState(false);
+  const [menuItems, setMenuItems] = useState([]);
+  const [fetchMenu, setFetchMenu] = useState(false);
+  const [selectedItems, setSelectedItems] = useState([]);
+
+  // Fetch menu items from the API when the checkbox is clicked
+  useEffect(() => {
+    if (fetchMenu) {
+      fetch(
+        "https://xt9806b6e1.execute-api.us-east-1.amazonaws.com/default/getMenuItems?restaurantId=12"
+      )
+        .then((response) => response.json())
+        .then((data) => setMenuItems(data[0].Items));
+    } else {
+      // If the checkbox is unchecked, clear the menu items
+      setMenuItems([]);
+    }
+  }, [fetchMenu]);
+
+  const handleCheckboxChange = () => {
+    setFetchMenu(!fetchMenu);
+  };
+
+  const handleItemSelect = (item) => {
+    // Check if the item is already in the selectedItems array
+    const index = selectedItems.findIndex((i) => i.item_id === item.item_id);
+
+    if (index !== -1) {
+      // If it's already selected, update the quantity
+      const updatedItems = [...selectedItems];
+      updatedItems[index].quantity = item.quantity;
+      setSelectedItems(updatedItems);
+    } else {
+      // If it's not selected, add it to the array
+      setSelectedItems([...selectedItems, item]);
+    }
+  };
+
+  const handleBookClick = () => {
+    // Print the selected items array
+    console.log(selectedItems);
+  };
 
   return (
-    <div>
-      <BookingForm />
-      <TimeSlotSelector />
-      <FormControlLabel
-        control={
-          <Checkbox
-            checked={isMenuSelected}
-            onChange={() => setIsMenuSelected(!isMenuSelected)}
-          />
-        }
-        label="Book Food From Menu (Optional)"
-      />
-      {isMenuSelected && <MenuSelector />}
-    </div>
+    <>
+      <Box m={2} p={2}>
+        <Typography variant="h4">Table Booking Page</Typography>
+        <FormControlLabel
+          control={
+            <Checkbox
+              checked={fetchMenu}
+              onChange={handleCheckboxChange}
+              color="primary"
+            />
+          }
+          label="Add Items From Menu to this Order(Optional)"
+        />
+        {fetchMenu && (
+          <Grid container spacing={2}>
+            {menuItems.map((item) => (
+              <Grid item xs={12} sm={6} md={4} lg={3} key={item.ItemID}>
+                <MenuItemCard item={item} onItemSelect={handleItemSelect} />
+              </Grid>
+            ))}
+          </Grid>
+        )}
+        <Box mt={2}>
+          <Button variant="contained" color="primary" onClick={handleBookClick}>
+            Book
+          </Button>
+        </Box>
+      </Box>
+      <Footer />
+    </>
   );
 }
 
