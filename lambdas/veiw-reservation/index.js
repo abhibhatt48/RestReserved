d0b528f16aea78888bce2b7c9ba1b79ca753295b
ASHISH OJHA <as950640@dal.ca>
2023-10-25 21:19:53 -0300

    updated lambdas with table number

--- lambdas/veiw-reservation/index.js
+++ lambdas/veiw-reservation/index.js
@@ -1,54 +1,59 @@
-const AWS = require('aws-sdk');
-const dynamodb = new AWS.DynamoDB.DocumentClient();
+const admin = require("firebase-admin");
+const serviceAccount = require("./high-radius-401215-firebase-adminsdk-c4bv1-1ba4657cbc.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://high-radius-401215.firebaseio.com",
+});
+
+const firestore = admin.firestore();
 
 exports.handler = async (event) => {
-    try {
-        const requestBody = JSON.parse(event.body);
-        const reservationId = requestBody.reservation_id;
+  try {
+    const requestBody = JSON.parse(event.body);
+    const reservationId = requestBody.reservation_id;
 
-        const reservation = await getReservationById(reservationId);
+    const reservation = await getReservationById(reservationId);
 
-        if (!reservation) {
-            return {
-                statusCode: 404,
-                body: JSON.stringify({ message: 'Reservation not found' }),
-            };
-        }
-
-        return {
-            statusCode: 200,
-            body: JSON.stringify({ message: 'Reservation found', reservation }),
-        };
-    } catch (error) {
-        console.error('Error in Lambda handler:', error);
-        return {
-            statusCode: 500,
-            body: JSON.stringify({ message: 'Failed to retrieve reservation' }),
-        };
+    if (!reservation) {
+      return {
+        statusCode: 404,
+        body: JSON.stringify({ message: "Reservation not found" }),
+      };
     }
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: "Reservation found", reservation }),
+    };
+  } catch (error) {
+    console.error("Error in Lambda handler:", error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: "Failed to retrieve reservation" }),
+    };
+  }
 };
 
 // Function to get a reservation by ID
 async function getReservationById(reservationId) {
-    try {
-        const params = {
-            TableName: 'restaurant_reservation', // Replace with your table name
-            Key: {
-                reservation_id: reservationId,
-            },
-        };
+  try {
+    const reservationRef = firestore
+      .collection("reservations")
+      .doc(reservationId);
+    const existingReservation = await reservationRef.get();
 
-        console.log('Getting reservation by ID:', reservationId);
-        const existingReservation = await dynamodb.get(params).promise();
-        
-        if (!existingReservation.Item) {
-            console.log('Reservation not found.');
-            return null; // Reservation not found
-        }
-
-        return existingReservation.Item;
-    } catch (error) {
-        console.error('Error getting reservation by ID:', error);
-        return null;
+    if (!existingReservation.exists) {
+      console.log("Reservation not found.");
+      return null; // Reservation not found
     }
+
+    return {
+      reservation_id: reservationId,
+      ...existingReservation.data(),
+    };
+  } catch (error) {
+    console.error("Error getting reservation by ID:", error);
+    return null;
+  }
 }
