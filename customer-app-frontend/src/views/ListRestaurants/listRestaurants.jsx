a4c67e12fe79f24f68797918ef25c15d3293e795
Sushank Saini <sushank.saini@dal.ca>
2023-10-26 21:30:41 -0300

    improved ui for list restaurants

--- customer-app-frontend/src/views/ListRestaurants/listRestaurants.jsx
+++ customer-app-frontend/src/views/ListRestaurants/listRestaurants.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Card, CardContent, Typography, Button,Grid } from '@mui/material';
 
 const RestaurantList = () => {
   const [restaurants, setRestaurants] = useState([]);
@@ -19,17 +20,26 @@ const RestaurantList = () => {
   }, []); // Empty dependency array to make sure that this effect runs once after the initial render
 
   return (
-    <div className="restaurant-list">
-      {restaurants.map((restaurant) => (
-        <div className="restaurant-card" key={restaurant.res_name}>
-          <h2>{restaurant.res_name}</h2>
-          <p>Location: {restaurant.res_location}</p>
-          <p>Opening Time: {restaurant.res_opening_time}</p>
-          <p>Closing Time: {restaurant.res_closing_time}</p>
-          <button className="reservation-button">Make a Reservation</button>
-        </div>
+    <Grid container spacing={2} justifyContent="center">
+      {restaurants.map((restaurant, index) => (
+        <Grid item key={index}>
+          <Card variant="outlined">
+            <CardContent>
+              <Typography variant="h6" component="div">
+                {restaurant.res_name}
+              </Typography>
+              <Typography color="text.secondary">{restaurant.res_location}</Typography>
+              <Typography color="text.secondary">
+                Opening Time: {restaurant.res_opening_time} - Closing Time: {restaurant.res_closing_time}
+              </Typography>
+              <Button variant="contained" color="primary">
+                Make a Reservation
+              </Button>
+            </CardContent>
+          </Card>
+        </Grid>
       ))}
-    </div>
+    </Grid>
   );
 };
 
