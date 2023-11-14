8cb5da68727e89832926e90d231f1e609d3b86b5
Sushank Saini <sushank.saini@dal.ca>
2023-11-13 23:24:06 -0400

    changed routes.js to add sign up page route

--- partner-app-frontend/src/routes.js
+++ partner-app-frontend/src/routes.js
@@ -1,9 +1,11 @@
 import { Routes, Route } from "react-router-dom";
 import Holistic from "./views/Holistic/index";
+import SignupForm from "./views/Authentication/restaurantSignUp";
+
 const Router = () => {
   return (
     <Routes>
-      <Route path="/" element={<Holistic />} />
+      <Route path="/" element={<SignupForm />} />
       <Route path="/holistic" element={<Holistic />} />
     </Routes>
   );
