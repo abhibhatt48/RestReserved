5c8219a49cd294cfc016070123848dc978a319e8
Sushank Saini <sushank.saini@dal.ca>
2023-11-14 00:03:14 -0400

    added sign in pages routes

--- partner-app-frontend/src/routes.js
+++ partner-app-frontend/src/routes.js
@@ -1,11 +1,15 @@
 import { Routes, Route } from "react-router-dom";
 import Holistic from "./views/Holistic/index";
 import SignupForm from "./views/Authentication/restaurantSignUp";
+import SignInWithEmailForm from "./views/Authentication/restaurantSignin";
+import SignInWithGmailPage from './views/Authentication/restaurantGmailSignIn';
 
 const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<SignupForm />} />
+      <Route path="/signin" element={<SignInWithEmailForm />} />
+      <Route path="/googleSignIn" element={<SignInWithGmailPage/>}/>
       <Route path="/holistic" element={<Holistic />} />
     </Routes>
   );
