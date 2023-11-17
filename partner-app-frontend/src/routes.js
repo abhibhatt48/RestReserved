2c17f6480ec1ac3dd3a9d70340a2e736be706dff
ASHISH OJHA <as950640@dal.ca>
2023-11-17 16:49:17 -0400

    updated partner app

--- partner-app-frontend/src/routes.js
+++ partner-app-frontend/src/routes.js
@@ -1,10 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import Holistic from "./views/Holistic/index";
+import ViewBooking from "./views/ViewBooking/ViewReservation";
+import EditReservation from "./views/EditBooking";
 const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<Holistic />} />
       <Route path="/holistic" element={<Holistic />} />
+      <Route path="/view" element={<ViewBooking />} />
+      <Route path="/edit/:reservationId" element={<EditReservation />} />
     </Routes>
   );
 };
