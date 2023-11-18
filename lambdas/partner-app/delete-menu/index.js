457777957e34be59b2758b8d83fc53c09d30f08a
Jinay Shah <jn851778@dal.ca>
2023-11-18 19:23:09 -0400

    Updated delete-menu lambda.

--- lambdas/partner-app/delete-menu/index.js
+++ lambdas/partner-app/delete-menu/index.js
@@ -1,40 +1,59 @@
 const AWS = require('aws-sdk');
 
-const dynamoDB = new AWS.DynamoDB();
+const dynamoDB = new AWS.DynamoDB.DocumentClient();
 
 exports.handler = async (event) => {
-    console.log('Received event:', JSON.stringify(event, null, 2));
-
-    const menuItemID = JSON.parse(event.body).MenuItemID;
-    console.log("menuItemId",menuItemID);
-    // Validate input
-    if (!menuItemID || typeof menuItemID !== 'string') {
-        console.log('Invalid or missing MenuItemID:', menuItemID);
-        return {
-            statusCode: 400,
-            body: JSON.stringify('Invalid or missing MenuItemID'),
-        };
-    }
-    const params = {
-        TableName: 'menu-table', 
-        Key: {
-            MenuItemID: {
-                S: menuItemID
-            }
-        }
-    };
-
     try {
-        await dynamoDB.deleteItem(params).promise();
+        const requestBody = JSON.parse(event.body);
+        const { menu_id, item_id } = requestBody;
+
+        // Retrieve the existing menu data
+        const getMenuParams = {
+            TableName: 'menu',
+            Key: {
+                menu_id: menu_id,
+            },
+        };
+
+        const existingMenu = await dynamoDB.get(getMenuParams).promise();
+
+        if (!existingMenu.Item) {
+            return {
+                statusCode: 404,
+                body: JSON.stringify('Menu not found'),
+            };
+        }
+
+        // Remove the item from the "items" array
+        const updatedItems = existingMenu.Item.items.filter(item => item.item_id !== item_id);
+        console.log("updatedItems",updatedItems);
+        // Update the menu with the modified "items" array
+        const updateMenuParams = {
+            TableName: 'menu',
+            Key: {
+                menu_id: menu_id,
+            },
+            UpdateExpression: 'SET #items = :updatedItems',
+            ExpressionAttributeNames: {
+                '#items': 'items',
+            },
+            ExpressionAttributeValues: {
+                ':updatedItems': updatedItems,
+            },
+            ReturnValues: 'ALL_NEW',
+        };
+
+        await dynamoDB.update(updateMenuParams).promise();
+
         return {
             statusCode: 200,
-            body: JSON.stringify('Item deleted successfully'),
+            body: JSON.stringify('Menu item deleted successfully'),
         };
     } catch (error) {
-        console.error('Error deleting item:', error);
+        console.error('Error deleting menu item:', error);
         return {
             statusCode: 500,
-            body: JSON.stringify('Error deleting item'),
+            body: JSON.stringify('Error deleting menu item'),
         };
     }
 };
