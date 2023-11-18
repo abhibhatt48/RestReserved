bb787569613c219634f1932eb6d9ec039c77a85b
Abhishek Bhatt <47849440+abhibhatt48@users.noreply.github.com>
2023-11-17 20:44:02 -0400

    modified lambda.

--- lambdas/partner-app/list-restaurants/index.js
+++ lambdas/partner-app/list-restaurants/index.js
@@ -1,25 +1,38 @@
 const AWS = require('aws-sdk');
-AWS.config.update({ region: 'Write aws region here' });
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 const tableName = 'table_reservation_app_restaurants';
 
-exports.handler = async (event, context) => {
-    const params = {
-        TableName: tableName
-    };
-
+exports.handler = async (event) => {
     try {
-        const data = await dynamoDb.scan(params).promise();
+       const{queryStringParameters}= event;
+      
+        const {id:restaurantId} = queryStringParameters;
+
+        const params = {
+            TableName: tableName,
+            Key: {
+                'restaurant_id': restaurantId
+            }
+        };
+
+        const result = await dynamoDb.get(params).promise();
+
+        if (!result.Item) {
+            return {
+                statusCode: 404,
+                body: JSON.stringify({ message: 'Restaurant not found' }),
+            };
+        }
+
         return {
             statusCode: 200,
-            body: JSON.stringify(data.Items)
+            body: JSON.stringify(result.Item),
         };
     } catch (error) {
-        console.error('Error fetching data from DynamoDB:', error);
+        console.error(error);
         return {
             statusCode: 500,
-            body: JSON.stringify({ error: 'Could not fetch data' })
+            body: JSON.stringify({ error: 'Could not retrieve restaurant' }),
         };
     }
 };
-
