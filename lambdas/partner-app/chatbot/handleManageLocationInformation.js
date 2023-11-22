f25cd8b5f635d0a2ab7528b00fa18cbf6bfcc2d5
vaidik1999 <vaidiknimavat1@gmail.com>
2023-11-22 11:57:11 -0400

    Adding integrated files with working functionality

--- lambdas/partner-app/chatbot/handleManageLocationInformation.js
+++ lambdas/partner-app/chatbot/handleManageLocationInformation.js
@@ -1,6 +1,97 @@
+const axios = require('axios');
+
 function generateManageLocationInformationResponse(actionType, newAddress) {
     const responseMessage = `The location information has been ${actionType} to ${newAddress}.`;
-
+    
+    console.log(actionType + " " + newAddress);
+    
+    if (actionType == 'View'){
+        
+    }
+    
+    else if (actionType == 'Update'){
+    
+        console.log("updating information");
+    
+        const payload = {
+        email: 'saini7@dal.ca',
+        address: newAddress,
+        };
+        
+        
+        // Make the API call to update the opening times
+        console.log('Making API call to update opening times:', payload);
+        return axios.post('https://wzdh0w0265.execute-api.us-east-1.amazonaws.com/prod/storeRestaurantDetails', payload)
+        .then((response) => {
+            console.log('API call response:', response.status, response.data);
+        
+            if (response.data === true) {
+                // Opening times updated successfully
+                const responseMessage = responseMessage;
+                console.log('Location updated successfully:', responseMessage);
+                return {
+                    sessionState: {
+                        dialogAction: {
+                            type: 'Close',
+                        },
+                        intent: {
+                            name: 'ManageLocationInformation',
+                            state: 'Fulfilled',
+                        },
+                    },
+                    messages: [
+                        {
+                            contentType: 'PlainText',
+                            content: responseMessage,
+                        },
+                    ],
+                };
+            } else {
+                // Opening times update failed
+                console.log('Failed to update location. Response data:', response.data);
+                return {
+                    sessionState: {
+                        dialogAction: {
+                            type: 'Close',
+                        },
+                        intent: {
+                            name: 'ManageLocationInformation',
+                            state: 'Failed',
+                        },
+                    },
+                    messages: [
+                        {
+                            contentType: 'PlainText',
+                            content: 'Failed to update opening hours. Please try again.',
+                        },
+                    ],
+                };
+            }
+        })
+        .catch((error) => {
+            // Handle API call error
+            console.error('Error calling API:', error.message);
+            return {
+                sessionState: {
+                    dialogAction: {
+                        type: 'Close',
+                    },
+                    intent: {
+                        name: 'ManageLocationInformation',
+                        state: 'Failed',
+                    },
+                },
+                messages: [
+                    {
+                        contentType: 'PlainText',
+                        content: 'Failed to update location. Please try again later.',
+                    },
+                ],
+            };
+        });
+    
+    }
+    
     return {
         sessionState: {
             dialogAction: {
@@ -14,7 +105,7 @@ function generateManageLocationInformationResponse(actionType, newAddress) {
         messages: [
             {
                 contentType: 'PlainText',
-                content: responseMessage,
+                content: "Please choose the proper action",
             },
         ],
     };
@@ -33,3 +124,39 @@ module.exports = async (event) => {
         body: JSON.stringify('Hello from Lambda!'),
     };
 };
+
+// function generateManageLocationInformationResponse(actionType, newAddress) {
+//     const responseMessage = `The location information has been ${actionType} to ${newAddress}.`;
+
+//     return {
+//         sessionState: {
+//             dialogAction: {
+//                 type: 'Close',
+//             },
+//             intent: {
+//                 name: 'ManageLocationInformation',
+//                 state: 'Fulfilled',
+//             },
+//         },
+//         messages: [
+//             {
+//                 contentType: 'PlainText',
+//                 content: responseMessage,
+//             },
+//         ],
+//     };
+// }
+
+// module.exports = async (event) => {
+//     const intentName = event.sessionState.intent.name;
+
+//     if (intentName === 'ManageLocationInformation') {
+//         const slots = event.sessionState.intent.slots;
+//         return generateManageLocationInformationResponse(slots.ActionType, slots.NewAddress);
+//     }
+
+//     return {
+//         statusCode: 200,
+//         body: JSON.stringify('Hello from Lambda!'),
+//     };
+// };
--- lambdas/partner-app/chatbot/handleManageReservations.js
+++ lambdas/partner-app/chatbot/handleManageReservations.js
@@ -1,5 +1,312 @@
-function generateManageReservationsResponse(actionType, reservationDetails) {
-    const responseMessage = `The reservation for ${reservationDetails} has been ${actionType}.`;
+const axios = require('axios');
+
+function generateManageReservationsResponse(actionType, reservationId) {
+    
+    const id = reservationId.interpretedValue;
+    
+    actionType = actionType.interpretedValue;
+    
+    if(actionType == "approve"){
+        const payload = {
+            reservation_id: id,
+            status: 'Y',
+        };
+    
+    
+        // Make the API call to update the opening times
+        return axios.post('https://vdvua9bvw8.execute-api.us-east-1.amazonaws.com/prod/edit-reservation', payload)
+            .then((response) => {
+                console.log('API call response:', response.status, response.data);
+    
+                if (response.status == 200) {
+                    // Opening times updated successfully
+                    const responseMessage = `Reservation with id ${id} successfully approved.`;
+                    return {
+                        sessionState: {
+                            dialogAction: {
+                                type: 'Close',
+                            },
+                            intent: {
+                                name: 'ManageReservations',
+                                state: 'Fulfilled',
+                            },
+                        },
+                        messages: [
+                            {
+                                contentType: 'PlainText',
+                                content: responseMessage,
+                            },
+                        ],
+                    };
+                } else {
+                    // Opening times update failed
+                    return {
+                        sessionState: {
+                            dialogAction: {
+                                type: 'Close',
+                            },
+                            intent: {
+                                name: 'ManageReservations',
+                                state: 'Failed',
+                            },
+                        },
+                        messages: [
+                            {
+                                contentType: 'PlainText',
+                                content: 'Failed to approve the reservation.',
+                            },
+                        ],
+                    };
+                }
+            })
+            .catch((error) => {
+                // Handle API call error
+                console.error('Error calling API:', error.message);
+                return {
+                    sessionState: {
+                        dialogAction: {
+                            type: 'Close',
+                        },
+                        intent: {
+                            name: 'ManageReservations',
+                            state: 'Failed',
+                        },
+                    },
+                    messages: [
+                        {
+                            contentType: 'PlainText',
+                            content: 'Failed to approve reservation. Please try again later.',
+                        },
+                    ],
+                };
+            });
+    }
+    else if(actionType == "reject"){
+        const payload = {
+            reservation_id: id,
+            status: 'N',
+        };
+    
+    
+        // Make the API call to update the opening times
+        console.log('Making API call to update opening times:', payload);
+        return axios.post('https://vdvua9bvw8.execute-api.us-east-1.amazonaws.com/prod/edit-reservation', payload)
+            .then((response) => {
+                console.log('API call response:', response.status, response.data);
+    
+                if (response.status == 200) {
+                    // Opening times updated successfully
+                    const responseMessage = `Reservation with id: ${id} rejected successfully`;
+                    return {
+                        sessionState: {
+                            dialogAction: {
+                                type: 'Close',
+                            },
+                            intent: {
+                                name: 'ManageReservations',
+                                state: 'Fulfilled',
+                            },
+                        },
+                        messages: [
+                            {
+                                contentType: 'PlainText',
+                                content: responseMessage,
+                            },
+                        ],
+                    };
+                } else {
+                    // Opening times update failed
+                    return {
+                        sessionState: {
+                            dialogAction: {
+                                type: 'Close',
+                            },
+                            intent: {
+                                name: 'ManageReservations',
+                                state: 'Failed',
+                            },
+                        },
+                        messages: [
+                            {
+                                contentType: 'PlainText',
+                                content: 'Failed to reject reservation. Please try again.',
+                            },
+                        ],
+                    };
+                }
+            })
+            .catch((error) => {
+                // Handle API call error
+                console.error('Error calling API:', error.message);
+                return {
+                    sessionState: {
+                        dialogAction: {
+                            type: 'Close',
+                        },
+                        intent: {
+                            name: 'ManageReservations',
+                            state: 'Failed',
+                        },
+                    },
+                    messages: [
+                        {
+                            contentType: 'PlainText',
+                            content: 'Failed to reject reservation. Please try again later.',
+                        },
+                    ],
+                };
+            });
+    }
+    else if(actionType == "delete"){
+        const payload = {
+            reservation_id: id
+        };
+    
+    
+        // Make the API call to update the opening times
+        console.log('Making API call to update opening times:', payload);
+        return axios.post('https://vdvua9bvw8.execute-api.us-east-1.amazonaws.com/prod/delete-reservation', payload)
+            .then((response) => {
+                console.log('API call response:', response.status, response.data);
+    
+                if (response.status == 200) {
+                    // Opening times updated successfully
+                    const responseMessage = `Reservation with id: ${id} deleted successfully`;
+                    return {
+                        sessionState: {
+                            dialogAction: {
+                                type: 'Close',
+                            },
+                            intent: {
+                                name: 'ManageReservations',
+                                state: 'Fulfilled',
+                            },
+                        },
+                        messages: [
+                            {
+                                contentType: 'PlainText',
+                                content: responseMessage,
+                            },
+                        ],
+                    };
+                } else {
+                    // Opening times update failed
+                    return {
+                        sessionState: {
+                            dialogAction: {
+                                type: 'Close',
+                            },
+                            intent: {
+                                name: 'ManageReservations',
+                                state: 'Failed',
+                            },
+                        },
+                        messages: [
+                            {
+                                contentType: 'PlainText',
+                                content: 'Failed to delete reservation. Please try again.',
+                            },
+                        ],
+                    };
+                }
+            })
+            .catch((error) => {
+                // Handle API call error
+                console.error('Error calling API:', error.message);
+                return {
+                    sessionState: {
+                        dialogAction: {
+                            type: 'Close',
+                        },
+                        intent: {
+                            name: 'ManageReservations',
+                            state: 'Failed',
+                        },
+                    },
+                    messages: [
+                        {
+                            contentType: 'PlainText',
+                            content: 'Failed to delete reservation. Please try again later.',
+                        },
+                    ],
+                };
+            });
+    }
+    else if(actionType == "edit"){
+        const payload = {
+            reservation_id: id,
+            status: 'N',
+        };
+    
+    
+        // Make the API call to update the opening times
+        return axios.post('https://vdvua9bvw8.execute-api.us-east-1.amazonaws.com/prod/edit-reservation', payload)
+            .then((response) => {
+                console.log('API call response:', response.status, response.data);
+    
+                if (response.status == 200) {
+                    // Opening times updated successfully
+                    const responseMessage = `Reservation with id: ${id} edited successfully`;
+                    return {
+                        sessionState: {
+                            dialogAction: {
+                                type: 'Close',
+                            },
+                            intent: {
+                                name: 'ManageReservations',
+                                state: 'Fulfilled',
+                            },
+                        },
+                        messages: [
+                            {
+                                contentType: 'PlainText',
+                                content: responseMessage,
+                            },
+                        ],
+                    };
+                } else {
+                    // Opening times update failed
+                    return {
+                        sessionState: {
+                            dialogAction: {
+                                type: 'Close',
+                            },
+                            intent: {
+                                name: 'ManageReservations',
+                                state: 'Failed',
+                            },
+                        },
+                        messages: [
+                            {
+                                contentType: 'PlainText',
+                                content: 'Failed to edit reservation. Please try again.',
+                            },
+                        ],
+                    };
+                }
+            })
+            .catch((error) => {
+                // Handle API call error
+                console.error('Error calling API:', error.message);
+                return {
+                    sessionState: {
+                        dialogAction: {
+                            type: 'Close',
+                        },
+                        intent: {
+                            name: 'ManageReservations',
+                            state: 'Failed',
+                        },
+                    },
+                    messages: [
+                        {
+                            contentType: 'PlainText',
+                            content: 'Failed to edit reservation. Please try again later.',
+                        },
+                    ],
+                };
+            });
+    }
 
     return {
         sessionState: {
@@ -8,13 +315,13 @@ function generateManageReservationsResponse(actionType, reservationDetails) {
             },
             intent: {
                 name: 'ManageReservations',
-                state: 'Fulfilled',
+                state: 'Elicit',
             },
         },
         messages: [
             {
                 contentType: 'PlainText',
-                content: responseMessage,
+                content: "Please choose valid action",
             },
         ],
     };
@@ -25,7 +332,7 @@ module.exports = async (event) => {
 
     if (intentName === 'ManageReservations') {
         const slots = event.sessionState.intent.slots;
-        return generateManageReservationsResponse(slots.ActionType, slots.ReservationDetails);
+        return generateManageReservationsResponse(slots.ActionType.value, slots.ReservationId.value);
     }
 
     return {
@@ -33,3 +340,40 @@ module.exports = async (event) => {
         body: JSON.stringify('Hello from Lambda!'),
     };
 };
+
+
+// function generateManageReservationsResponse(actionType, reservationDetails) {
+//     const responseMessage = `The reservation for ${reservationDetails} has been ${actionType}.`;
+
+//     return {
+//         sessionState: {
+//             dialogAction: {
+//                 type: 'Close',
+//             },
+//             intent: {
+//                 name: 'ManageReservations',
+//                 state: 'Fulfilled',
+//             },
+//         },
+//         messages: [
+//             {
+//                 contentType: 'PlainText',
+//                 content: responseMessage,
+//             },
+//         ],
+//     };
+// }
+
+// module.exports = async (event) => {
+//     const intentName = event.sessionState.intent.name;
+
+//     if (intentName === 'ManageReservations') {
+//         const slots = event.sessionState.intent.slots;
+//         return generateManageReservationsResponse(slots.ActionType, slots.ReservationDetails);
+//     }
+
+//     return {
+//         statusCode: 200,
+//         body: JSON.stringify('Hello from Lambda!'),
+//     };
+// };
