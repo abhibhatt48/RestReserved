d76a48451f58af1f97aaf6bbe01e197dd70a12b3
Sushank Saini <sushank.saini@dal.ca>
2023-12-02 17:56:16 -0400

    added frontend page for top 10 customers

--- admin-app-frontend/src/routes.js
+++ admin-app-frontend/src/routes.js
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import SignInWithEmailForm from "./views/Authentication/adminSignin";
 import TopMenuItems from "./views/Reports/TopMenuItems";
+import TopCustomers from "./views/Reports/TopCustomers";
 const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<SignInWithEmailForm />} />
       <Route path="/top-items" element={<TopMenuItems />} />
+      <Route path="/top-customers" element={<TopCustomers />} />
     </Routes>
   );
 };
