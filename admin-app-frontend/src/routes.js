c0c05b430543d195029125cde15b419769fc05fb
ASHISH OJHA <as950640@dal.ca>
2023-12-02 20:49:20 -0400

    Fixed Build Issue

--- admin-app-frontend/src/routes.js
+++ admin-app-frontend/src/routes.js
@@ -4,15 +4,16 @@ import TopRestaurants from "./views/Reports/TopRestaurants";
 import TopMenuItems from "./views/Reports/TopMenuItems";
 import TopCustomers from "./views/Reports/TopCustomers";
 import Reviews from "./views/Reports/Reviews";
+import TopPeriods from "./views/Reports/TopPeriods";
 const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<SignInWithEmailForm />} />
-       <Route path="/top-restaurants" element={<TopRestaurants />} />
+      <Route path="/top-restaurants" element={<TopRestaurants />} />
       <Route path="/top-items" element={<TopMenuItems />} />
       <Route path="/top-customers" element={<TopCustomers />} />
-       <Route path="/reviews" element={<Reviews />} />
-       <Route path="/top-periods" element={<TopPeriods />} />
+      <Route path="/reviews" element={<Reviews />} />
+      <Route path="/top-periods" element={<TopPeriods />} />
     </Routes>
   );
 };
